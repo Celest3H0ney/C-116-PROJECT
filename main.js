dfe7938e29db3f103d72ec3669b1057a49608d52
Celest3H0ney <104111888+Celest3H0ney@users.noreply.github.com>
2023-01-11 19:45:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+mustacheX=0;
+mustacheY=0;
+
+function preload() {
+   mustache = loadImage('https://i.postimg.cc/q7YBXNRy/mustache-removebg-preview.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+      console.log(results);
+      noseX = results[0].pose.mustache.x, 50;
+      noseY = results[0].pose.mustache.y, 100;
+    } 
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache, 100, 100, 100, 100);
+}
+
+function take_snapshot() {
+    save('mustacheFilterImage.png');
+}
